998065214596531f9d50f8ef10941626560078ee
Undisclosed <undisclosed674@gmail.com>
2021-08-29 12:14:48 -0700

    render Basic Info component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import BasicInfo from './components/Basic Info';
+
+
+class App extends Component{
+render(){
+    return(
+        <div>
+
+<BasicInfo/>
+
+            </div>
+           
+    )
+}
+}
+export default App;
