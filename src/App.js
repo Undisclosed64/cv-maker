6970d413bee65c4ff01d1c9402b1bccdd1331c4b
Undisclosed <undisclosed674@gmail.com>
2021-08-30 00:47:07 -0700

    render more components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import BasicInfo from './components/Basic Info';
-
+import Education from './components/Education';
+import WorkExp from './components/WorkExp';
+import HandleBasicInfo from   './components/HandleBasicInfo'
+import HandleEducation from './components/HandleEducation';
+import HandleWorkExp from './components/HandleWorkExp';
 
 class App extends Component{
 render(){
     return(
         <div>
-
+    
 <BasicInfo/>
-
-            </div>
+<Education />
+<WorkExp />
+<HandleBasicInfo/>
+<HandleEducation/>
+<HandleWorkExp/>
+</div>
            
     )
 }
