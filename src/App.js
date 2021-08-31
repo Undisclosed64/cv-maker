cb53cf8d64f59c609cfe45830155d5ac15c9c1dd
Undisclosed <undisclosed674@gmail.com>
2021-08-31 06:58:11 -0700

    render components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import BasicInfo from './components/Basic Info';
+import './App.css'
+import Header from './components/Header';
+import UserForm from './components/UserForm';
+/*import BasicInfo from './components/Basic Info';
 import Education from './components/Education';
 import WorkExp from './components/WorkExp';
+*/
 import HandleBasicInfo from   './components/HandleBasicInfo'
 import HandleEducation from './components/HandleEducation';
 import HandleWorkExp from './components/HandleWorkExp';
+
+
 
 class App extends Component{
 render(){
     return(
         <div>
-    
-<BasicInfo/>
-<Education />
-<WorkExp />
+    <Header></Header>
+{/*<BasicInfo/>
+<Education/>
+<WorkExp/>
+<HandleBasicInfo/>
+*/}
+<UserForm/>
 <HandleBasicInfo/>
 <HandleEducation/>
 <HandleWorkExp/>
+
 </div>
            
     )
 }
 }
+
 export default App;
