2a8021e702d28681a7cb47874f8409229fbc4082
Undisclosed <undisclosed674@gmail.com>
2021-09-02 12:43:41 -0700

    add updated app.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import './App.css'
 import Header from './components/Header';
 import UserForm from './components/UserForm';
-import CVTheme from './components/CVTheme';
-/*import BasicInfo from './components/Basic Info';
-import Education from './components/Education';
-import WorkExp from './components/WorkExp';
-*/
+
 
 class App extends Component{
 render(){
     return(
         <div>
     <Header></Header>
-    
-<UserForm/>
+    <UserForm/>
 </div>
            
     )
