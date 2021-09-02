8eb12e9dcdf4e007b9b9004cc9cf0a8d70b884fe
Undisclosed <undisclosed674@gmail.com>
2021-09-01 23:32:58 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,31 +2,19 @@ import React, { Component } from 'react';
 import './App.css'
 import Header from './components/Header';
 import UserForm from './components/UserForm';
+import CVTheme from './components/CVTheme';
 /*import BasicInfo from './components/Basic Info';
 import Education from './components/Education';
 import WorkExp from './components/WorkExp';
 */
-import HandleBasicInfo from   './components/HandleBasicInfo'
-import HandleEducation from './components/HandleEducation';
-import HandleWorkExp from './components/HandleWorkExp';
-
-
 
 class App extends Component{
 render(){
     return(
         <div>
     <Header></Header>
-{/*<BasicInfo/>
-<Education/>
-<WorkExp/>
-<HandleBasicInfo/>
-*/}
+    
 <UserForm/>
-<HandleBasicInfo/>
-<HandleEducation/>
-<HandleWorkExp/>
-
 </div>
            
     )
