a5c50da7ff769ebde04575b1965fc9d9cf3e259f
Tahera Alam <72087614+Undisclosed64@users.noreply.github.com>
2023-04-07 21:37:04 -0700

    add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiFillBuild } from "react-icons/ai";
 
 const Header = () => {
   return (
     <div>
       <header className="flex justify-between px-1 py-4 md:px-10 items-center">
-        <div className="font-bold text-2xl md:text-3xl">ResumeForge</div>
+        <div className="font-bold text-2xl md:text-3xl flex items-center gap-2">
+          <AiFillBuild />
+          <div> ResumeForge</div>
+        </div>
         <div className="btn-wrapper rounded-full text-brightWhite bg-brightPurple px-4 py-3 flex items-center gap-2 font-medium">
           <a className="" href="#create-resume">
             Create Resume
